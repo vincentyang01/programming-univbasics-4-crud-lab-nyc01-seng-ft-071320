29894132b1b3608d00dfaced29fa25346d8e515c
Vincent Yang <vincentyangcsi@gmail.com>
2020-07-01 05:23:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  Array = ['a', 'b', 'c', 'd']
+  Array.push()
 end
 
 def add_element_to_start_of_array(array, element)
